ea789b94894f46e377a1e450b09b0c1b32682dce
perseus-sergey <127436603+perseus-sergey@users.noreply.github.com>
2023-07-27 10:39:23 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// import './template.html';
+import './index.scss';
+import { App } from './app/app';
+
+import './assets/images/rs-school.png';
+import './assets/images/ukraine_stand.png';
+
+const app = new App();
+app.start();
