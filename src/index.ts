56ea642c2124b69293530833d3f9dc7ccfa0f5e9
perseus <sergeygubriy@gmail.com>
2023-07-30 02:54:58 +0300

    docs: finish task

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-// import './template.html';
-import './index.scss';
-import { App } from './app/app';
-
-import './assets/images/rs-school.png';
-import './assets/images/ukraine_stand.png';
-
-const app = new App();
-app.start();
